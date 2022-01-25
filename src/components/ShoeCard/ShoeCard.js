ef055840beb1d4f2ce6184afaafb9132369c628e
PaulinaPaw <paulinapatrycja.fior@gmail.com>
2022-01-25 19:43:08 +0100

    Exercise 4

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -53,6 +53,8 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  width: 340px;
+  flex: 1 1 auto;
 `;
 
 const Wrapper = styled.article``;
@@ -61,7 +63,9 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
